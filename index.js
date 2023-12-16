4909834a7cb668464a33ec785150f89a4c4a96d6
hammahb <hammahb84@gmail.com>
2023-12-16 14:52:18 +0100

    updated

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 function  weirddivisionwhithfor(n){
-    if (n%3){
-    return julia }
-        if (n%5){
-        return james }
-        if (n%3,5){
-            return (n)
+    if (n%3===0){
+    return "julia" }
+        if (n%5===0){
+        return "james" }
+        if (n%3===0 && n%5===0){
+            return "juliajames"
+        }else{
+            return n
         }
         
 
-        function iterateandlogwithfor(n){
-    for(var i = 0 ;i < n; i++){
-        if (i/2= 0)
-        console.log  even}
-
-        console.log odd 
+        function iterateandlogWithfor(n){
+    var msg=""
+    for(var i=0;i<n;i++){
+        if(i%2===0){
+            console.log(msg=i+" is even")
         }
+        else{
+            console.log(msg=i+" is odd")
+        }
+    }
+}
+function iterateandlogWithwhile(n){
+    var msg=""
+    var i=0
+    while(i<n){
+        if(i%2===0){
+            console.log(msg=i+" is even")
+            i++
+        }
+        else{
+            console.log(msg=i+" is odd")
+            i++
+        }
+    }
+}
