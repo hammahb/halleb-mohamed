616c285d229f0c6e351ea001a9f64bd3ae210ca5
hammahb <hammahb84@gmail.com>
2023-12-16 12:06:12 +0100

    done

---
+++ index.js
@@ -0,0 +1,17 @@
+function  weirddivisionwhithfor(n){
+    if (n%3){
+    return julia }
+        if (n%5){
+        return james }
+        if (n%3,5){
+            return (n)
+        }
+        
+
+        function iterateandlogwithfor(n){
+    for(var i = 0 ;i < n; i++){
+        if (i/2= 0)
+        console.log  even}
+
+        console.log odd 
+        }
